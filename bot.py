b2937c96b6fc427a81613f503ab0a70461bf0053
Sogata <maximer819@gmail.com>
2024-11-11 21:20:37 +0100

    Correction bug

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import typing
 import os
 
 import discord
+import pytubefix.exceptions
 from discord.ext import commands
 
 import captcha as c
@@ -214,14 +215,14 @@ async def play(interaction: discord.Interaction, url: str):
             vc = await state.channel.connect()
             try:
                 embed = await p.add_audio(interaction, url, 0, settings)
-            except discord.app_commands.errors.CommandInvokeError:
+            except pytubefix.exceptions.BotDetection:
                 await interaction.channel.send(":warning: le bot ne peut actuellement pas lancer l'audio")
                 await vc.disconnect()
             await interaction.response.send_message(embed=embed)
         else:
             try:
                 embed = await p.add_audio(interaction, url, 1, settings)
-            except discord.app_commands.errors.CommandInvokeError:
+            except pytubefix.exceptions.BotDetection:
                 await interaction.channel.send(":warning: le bot ne peut actuellement pas lancer l'audio")
                 await interaction.guild.voice_client.disconnect()
 
