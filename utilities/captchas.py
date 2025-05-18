7ab51ab20e8b4ce23deb67d9bcb516d14bee08e1
sogata-aw <maximer819@gmail.com>
2025-05-18 16:40:38 +0200

    Augmentation de la taille de l'image du captcha et de la taille du texte

--- utilities/captchas.py
+++ utilities/captchas.py
@@ -6,9 +6,11 @@ global_fonts = [os.path.join(os.getcwd(), 'fonts', 'arial.ttf'), os.path.join(os
              os.path.join(os.getcwd(), 'fonts', 'times.ttf'),
              os.path.join(os.getcwd(), 'fonts', 'verdana.ttf')]
 
+font_sizes = (80,82,84,86,88,90)
+
 def generer_image(code = "123456"):
     fonts = global_fonts
-    image = ImageCaptcha(fonts=fonts)
+    image = ImageCaptcha(fonts=fonts, width=250, height=100, font_sizes=font_sizes)
     image.write(code,os.path.join(os.getcwd(),'img','captcha.png'))
 
 def generer_code(taille=6):
