7fe03e1561493c8a6ae845092347a1c2117152c5
Sogata <maximer819@gmail.com>
2026-01-09 17:12:21 +0100

    BUG CORRIGE TOUT ÇA A CAUSE D'UNE PARENTHESE MAL FERMEE

--- utilities/dater.py
+++ utilities/dater.py
@@ -6,18 +6,18 @@ import discord
 import bdd
 
 
-async def mois_en_jours(mois: int):
+def mois_en_jours(mois: int):
     return mois * 30
 
 
-async def annee_en_jours(annee: int):
+def annee_en_jours(annee: int):
     return mois_en_jours(12 * annee)
 
 
 async def create_channel_duree(interaction: discord.Interaction, nom: str, typesalon: str, guilds: dict[int, bdd.GuildData], categorie: discord.CategoryChannel = None, duree: str = None):
     if typesalon == "textuel":
-        salon_temp = await interaction.guild.create_text_channel(name=nom, category=categorie)
-        bdd.add_temp_channel(guilds, interaction, salon_temp, nom, typesalon, categorie, duree)
+        salon_temp = await interaction.guild.create_text_channel(name=nom.lower(), category=categorie)
+        bdd.add_temp_channel(guilds, interaction, salon_temp, nom.lower(), typesalon, categorie, duree)
         await interaction.response.send_message(
             embed=discord.Embed(title=":white_check_mark: Le salon a été crée", colour=0x008001))
     elif typesalon == "vocal":
@@ -39,13 +39,13 @@ async def create_role_duree(interaction: discord.Interaction, nom: str, duree: s
 async def ajouter_temps(duree_split: list[str]):
     duration = d.datetime.now()
     duree_split = str_to_int(duree_split)
-    jours = semaines = heures = minutes = 0
+    jours = heures = minutes = 0
     for i in range(len(duree_split)):
         if duree_split[i] in ['an', 'ans']:
-            jours += annee_en_jours(duree_split[i - 1])
+            jours += mois_en_jours(int(duree_split[i - 1]))
         elif duree_split[i] == 'mois':
-            jours += annee_en_jours(duree_split[i - 1])
-        elif duree_split[i] in ['j,jours,jour']:
+            jours += annee_en_jours(int(duree_split[i - 1]))
+        elif duree_split[i] in ['j', 'jours', 'jour']:
             jours += duree_split[i - 1]
         elif duree_split[i] in ['h', 'heure', 'heures']:
             heures += duree_split[i - 1]
@@ -53,7 +53,7 @@ async def ajouter_temps(duree_split: list[str]):
             minutes += duree_split[i - 1]
         elif i == (len(duree_split) - 1) and isinstance(duree_split[i], int):
             minutes += duree_split[i]
-    duration += d.timedelta(weeks=semaines, days=jours, hours=heures, minutes=minutes)
+    duration += d.timedelta(days=jours, hours=heures, minutes=minutes)
     return duration
 
 
